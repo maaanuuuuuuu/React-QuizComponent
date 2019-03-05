859e41fee8b24076623322da1eaea41f74254802
Désir <emmanuel.desir@enablon.com>
2019-03-05 17:59:09 +0100

    Completed the module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { incorrectAnswer: false }
+  }
   handleClick(buttonText) {
     console.log('QuizQuestion>handleClick')
     console.log(buttonText)
     console.log(this.props.quiz_question.answer)
     if (buttonText === this.props.quiz_question.answer) {
+      this.setState((state) => {
+        return {
+          incorrectAnswer: false
+        }
+      })
       this.props.showNextQuestionHandler()
+    } else {
+      this.setState((state) => {
+        return {
+          incorrectAnswer: true
+        }
+      })
     }
   }
   render(h) {
@@ -25,6 +40,10 @@ class QuizQuestion extends Component {
             })}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? 
+          <p className="error"> Sorry, that's not right </p> : null
+        }
+        
       </main>
     )
   }
