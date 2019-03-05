92e50ecc559e21d67b00f4c70a989a48086a7753
Désir <emmanuel.desir@enablon.com>
2019-03-05 17:44:07 +0100

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,13 +8,21 @@ class Quiz extends Component {
     super(props)
     this.state = { quiz_position: 1 }
   }
+  showNextQuestion() {
+    console.log('Quiz>showNextQuestion')
+    this.setState((state) => {
+      return {
+        quiz_position: state.quiz_position + 1
+      }
+    })
+  }
   render(h) {
     const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length
     return (
       <div>
         {isQuizEnd ? 
           <QuizEnd /> :
-          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+          <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
         }
       </div>
     )
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 
 
 class QuizQuestionButton extends Component {
-  constructor(props) {
-    super(props)
+  handleClick() {
+    console.log('QuizQuestionButton>handleClick')
+    this.props.clickHandler(this.props.button_text)
   }
   render(h) {
     return (
       <li>
-        <button>{ this.props.button_text }</button>
+        <button onClick={this.handleClick.bind(this)}>{ this.props.button_text }</button>
       </li>
     )
   }
